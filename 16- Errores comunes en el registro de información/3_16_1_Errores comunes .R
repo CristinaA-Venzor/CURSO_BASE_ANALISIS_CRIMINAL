fe01bbb400fc8d869bdb94388f0d15afe095eb7f
JulianZpin <141749640+JulianZpin@users.noreply.github.com>
2023-08-14 23:33:18 -0600

    3_16_1_Errores comunes .R

--- 16- Errores comunes en el registro de información/3_16_1_Errores comunes .R
+++ 16- Errores comunes en el registro de información/3_16_1_Errores comunes .R
@@ -2,7 +2,6 @@
 # Errores comunes en el registro de información 
 ############################################
 
-
 #############
 rm(list=ls())                                   # Limpiar
 setwd()  # Directorio/Carpeta a utilizar especificado dentro de los parentesis
@@ -16,50 +15,37 @@ library(dplyr)
 library(janitor)
 #install.packages("usethis")
 library("usethis")
-
-#Verificar el ambiente de github en donde está trabajando
-usethis::git_sitrep()
-#Verificar la contresaña que debe se rigual a la de abajo
-gitcreds::gitcreds_set()
-#establecer el token o path que te da acceso al repositorio.
-credentials::set_github_pat("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")
+library(datasets)
 
 # Desde archivos separados por delimitador (como CSV)
-
-delitos <- read.csv("https://raw.githubusercontent.com/CristinaA-Venzor/CURSO_BASE_ANALISIS_CRIMINAL/main/Bases%20de%20datos/fresnillo.csv?token=GHSAT0AAAAAACCJCBZJCPSDOZ5FNJZNQGWEZGJFLJA")
+delitos <- read.csv("https://raw.githubusercontent.com/CristinaA-Venzor/CURSO_BASE_ANALISIS_CRIMINAL/main/Bases%20de%20datos/carpetas_2023.csv")
 
 # Veo las diemnsiones de la base de datos
-
 dim(delitos)
 
-# Borro valores duplicados en el el identificador CUI
-delitos <- delitos[!duplicated(delitos[ ,3]), ]  # Delete rows
+# Borro valores duplicados cuando concuerdan en las columnas dentro de c
+delitos <- delitos[!duplicated(delitos[ ,c(4, 8, 11, 17)]), ]  # Delete rows
  
 #reviso las nuevas dimensiones
 dim(delitos)
 
-#vemos que había una línea repetida en el identificador único, lo cuál ahora tenemos
-#342 eventos.
+#vemos que había líneas repetidas, por lo cuál ahora tenemos 65333 eventos.
 
-#Contar Número de NAs- número de registros que no tiene información en comunidad
-sum(is.na(delitos$Comunidad))
+#Contar Número de NAs: número de registros que no tiene información en datos de la colonia
+sum(is.na(delitos$colonia_datos))
 
-#contar valores faltantes en Sexo de la victima
-sum(is.na(delitos$Edad.de.la.víctima))
+data(iris) # Recurro a una base de datos dentro de R
 
-# Como faltan valores, calcilo el promedio de la edad y los imputo en los faltantes
-delitos$Edad.de.la.víctima[is.na(delitos$Edad.de.la.víctima)] <- mean(delitos$Edad.de.la.víctima,na.rm=TRUE)
-
-#contar valores faltantes en Sexo de la victima
-sum(is.na(delitos$Edad.de.la.víctima))
+# Supongo que faltan valores, calculo el promedio Petal.Width sin considerar valores nulos
+iris$Petal.Width[is.na(iris$Petal.Width)]
+mean(iris$Petal.Width,na.rm=TRUE)
 
 ##########################################
 #EJERCICIO
 
-#1) Cuántos valores faltantes hay en la columna "Colonia"?
-#2) Elimina los valores faltentes en Colonia
+#1) Cuántos valores faltantes hay en la columna "municipio_hechos"?
+#2) Elimina los valores faltentes en municipio_hechos
 #3) Eliina la columna si tiene más de la mitad de valores flatantes.
 
 #sube tu este Script contestando las preguntas a esta liga:
 # https://forms.gle/G8aJyP6vKVbYVTkb9
-
