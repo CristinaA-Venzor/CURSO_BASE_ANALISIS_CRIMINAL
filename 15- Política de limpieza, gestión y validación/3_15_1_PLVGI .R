7cc2f8791b65c8e2a4b8a75d9c561d9b5a4b7f84
JulianZpin <141749640+JulianZpin@users.noreply.github.com>
2023-08-14 23:26:25 -0600

    3_15_1_PLVGI .R

--- 15- Política de limpieza, gestión y validación/3_15_1_PLVGI .R
+++ 15- Política de limpieza, gestión y validación/3_15_1_PLVGI .R
@@ -2,7 +2,6 @@
 # Limpieza de datos en R
 ############################################
 
-
 #############
 rm(list=ls())                                   # Limpiar
 setwd()  # Directorio/Carpeta a utilizar especificado dentro de los parentesis
@@ -16,20 +15,20 @@ library(dplyr)
 library(janitor)
 
 # Desde archivos separados por delimitador (como CSV)
-delitos <- read.csv("https://raw.githubusercontent.com/CristinaA-Venzor/CURSO_BASE_ANALISIS_CRIMINAL/main/Bases%20de%20datos/fresnillo.csv?token=GHSAT0AAAAAACCJCBZJMLVQOMPBSFBBHPMAZGIISHQ")
+delitos <- read.csv("https://raw.githubusercontent.com/CristinaA-Venzor/CURSO_BASE_ANALISIS_CRIMINAL/main/Bases%20de%20datos/carpetas_2023.csv")
 
 # Básico
 #Estandarizar nombre de variables
 # revisamos nombres de variables
 colnames(delitos)
-# re-escribios las primeras 5 nombres de variables 
+# re-escribios 3 nombres de variables 
 
 #criterios: minúsculas, sin acentos, sin espacios, corta
-colnames(delitos)[1] <- "marca_temporal"
-colnames(delitos)[2] <- "num_cosecutivo"
-colnames(delitos)[3] <- "CUI"
-colnames(delitos)[4] <- "num_interno"
-colnames(delitos)[5] <- "fecha_rep_OP"
+colnames(delitos)[1] <- "año_hechos"
+colnames(delitos)[5] <- "año_inicio"
+colnames(delitos)[13] <- "categoria_del_delito"
+
+colnames(delitos)
 
 #Otra manera de limpiar todos los nombres de las variables 
 delitos <- clean_names(delitos)
@@ -37,13 +36,19 @@ delitos <- clean_names(delitos)
 # revisamos nombres de variables
 colnames(delitos)
 
-#Eliminar acentos en calle y número 
+# Eliminar acentos en categoria del delito --------------------------------
 
-delitos$`Calle y Numero` <- chartr("ÁÉÍÓÚ", "AEIOU", toupper(delitos$`Calle y Numero`))
+# Definir una función para reemplazar acentos por caracteres sin acento
+quitar_acentos <- function(texto) {
+  texto <- iconv(texto, to = "ASCII//TRANSLIT")
+  return(texto)
+}
 
+# Aplicar la función a la columna 'texto' del data frame
+delitos$categoria_del_delito <- sapply(delitos$categoria_del_delito, quitar_acentos)
 
 #Sustituir espacios en blanco por NA en columna comunidad
-delitos$Comunidad[delitos$Comunidad == ""] <- NA 
+delitos$colonia_datos[delitos$colonia_datos == ""] <- NA 
 
 ##########################################
 #EJERCICIO
@@ -51,9 +56,8 @@ delitos$Comunidad[delitos$Comunidad == ""] <- NA
 #1) cambia el nombre de otras 5 columnas con los criterios mencionados
 #2) elimina los acentos de otra columna que consideres necesario
 #3) cambia la "ñ" por n en la columna de "fórmula dirección"
-#4) sustituye espacios por NA en "colina", "cruce", "tipo de lugar" y "punto de referencia"
+#4) sustituye espacios por NA en don de consideres necesario
 #5) cuántos registros no tienen información de punto de referencia (NA)?
 
 #sube tu este Script contestando las preguntas a esta liga:
 # https://forms.gle/dPZVGAmpuG7mVQ3ZA
-
