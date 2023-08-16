e2ddfd9475c870472c6581719f1f6f0e27d46a88
CristinaA-Venzor <cristinaa@zpin.tech>
2023-08-16 11:06:07 -0600

    Update 3_26_Algoritmo SmartEDA.R

--- 26- Algoritmo SMARTEDA/3_26_Algoritmo SmartEDA.R
+++ 26- Algoritmo SMARTEDA/3_26_Algoritmo SmartEDA.R
@@ -1,6 +1,6 @@
 # La primera vez que se ejecuta el código, es relevante instalar los paquetes
 # install.packages("devtools")
-# devtools::install_github("agstn/dataxray")
+#devtools::install_github("agstn/dataxray")
 
 library(tidyverse)
 library(dataxray)
@@ -13,3 +13,13 @@ delitos <- read.csv("https://raw.githubusercontent.com/CristinaA-Venzor/CURSO_BA
 delitos %>%
   make_xray() %>%
   view_xray() # Genero y visualizo mi xray donde se podran observar datos estadidisticos (medidas de tendencia central), sus respectivas graficas e informacion sobre el tipo de datos con que estoy trabajando
+
+
+# Visualiza la descrpipción de cada variable
+# Replica este script con la base de datos indicada
+# expande la visualizaición y analiza:
+  #1. ¿Para que es útil tener esta información?
+  #2. ¿Cómo sirve este comando para la Política de Gestión, Validación y Limpieza de datos?
+
+
+  
