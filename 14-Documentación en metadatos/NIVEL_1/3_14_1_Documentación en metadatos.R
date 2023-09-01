09b3a3d958e30cd7a67db01142ce5e04e60c4ff4
JulianZpin <141749640+JulianZpin@users.noreply.github.com>
2023-08-31 21:55:13 -0600

    3_14_1_Documentación en metadatos.R

--- 14-Documentación en metadatos/NIVEL_1/3_14_1_Documentación en metadatos.R
+++ 14-Documentación en metadatos/NIVEL_1/3_14_1_Documentación en metadatos.R
@@ -1,24 +1,11 @@
-# La primera vez que se ejecuta el código, es relevante instalar los paquetes
-#install.packages("kableExtra")
-#install.packages("summarytools")
-#install.packages("flextable")
-#install.packages("gt")
-#install.packages("pander")
-#library(summarytools)
+## Instalar librerías
+# Quitar el signo de gato y correr, sólo la primera vez que se utilice el paquete en la computadora
+# install.packages("magrittr")
+# install.packages("flextable")
 
-
-#library(dplyr)
-library(SmartEDA)
-library(knitr)
-library(kableExtra)
-library(readxl)
+## Activar librerías
 library(flextable)
-library(janitor)
-library(descr)
-#library(gt)
-library(pander)
-library("usethis")
-
+library(magrittr)
 
 # Nombre.csv es la base de datos que se va a documentar (en este caso un vinculo)
 df_base<-read.csv("https://raw.githubusercontent.com/CristinaA-Venzor/CURSO_BASE_ANALISIS_CRIMINAL/main/Bases%20de%20datos/carpetas_2023.csv")
@@ -29,10 +16,8 @@ tabla_final<-SmartEDA::ExpData(df_base,type=2)
 # Cambio lo valores de las gráficas
 colnames(tabla_final)<-c("No.","Nombre de variable", "Tipo de datos", "Número de registros", "Valores faltantes", "Porcentaje de valores faltantes", "Número de valores distintos")
 
-
 # Exporto en documento de word
 flextable(tabla_final) %>% save_as_docx( path = "metadatos_nivel_1.docx") # Se guarda en la carpeta de trabajo "getwd()"
-
 
 # Doy formato a la tabla y la genero en mi viewer
 tabla_final<-flextable::flextable(tabla_final)
